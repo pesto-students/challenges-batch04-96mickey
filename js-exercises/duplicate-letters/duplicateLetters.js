3daed7b9334a7605384caa1c6d7cd0677148c04a
Mayank <mayank.nauriyal@sourcefuse.com>
2020-10-21 18:48:54 +0530

    Solution for duplicate letters exercise
    Signed-off-by: Mayank <mayank.nauriyal@sourcefuse.com>

---
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -0,0 +1,30 @@
+function duplicateLetters(val) {
+  if (typeof val !== 'string') {
+    throw new Error('Input must be a string !');
+  }
+
+  // creating a cache memory to keep record of alphabets and their concurrency
+  const cache = {};
+  const arrOfAlphabets = val.split('');
+  arrOfAlphabets.forEach(alphabet => {
+    if (cache[alphabet]) {
+      cache[alphabet] += 1;
+    } else {
+      cache[alphabet] = 1;
+    }
+  });
+  // sorting the data to find concurrency in decreasing order
+  const sortedAlphabet = Object.keys(cache).sort((a, b) => cache[b] - cache[a]);
+
+  // If the largest concurrency greater than 1, return the number
+  if (cache[sortedAlphabet[0]] > 1) {
+    return cache[sortedAlphabet[0]];
+  // eslint-disable-next-line no-else-return
+  } else {
+    return false;
+  }
+}
+
+export {
+  duplicateLetters,
+};
